127c2654fe3c604de2b88bab97f152fccd102a92
d1sam <d1sam.dot@gmail.com>
2023-05-26 13:10:17 +0300

    fix 3 commit

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -4,31 +4,40 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class TodosTest {
     private Todos todos;
+    private SimpleTask t1;
+    private Epic t2;
+    private Meeting t3;
 
     @BeforeEach
     public void setUp() {
+        t1 = new SimpleTask(1, "Купить продукты");
+        t2 = new Epic(2, new String[]{"Написать код", "Протестировать код"});
+        t3 = new Meeting(3, "Проверить код", "Project X", "1 июня 2023, 12-00");
         todos = new Todos();
-        todos.add(new SimpleTask(1, "Купить продукты"));
-        todos.add(new Epic(2, new String[]{"Написать код", "Протестировать код"}));
-        todos.add(new Meeting(3, "Проверить код", "Project X", "1 июня 2023, 12-00"));
+        todos.add(t1);
+        todos.add(t2);
+        todos.add(t3);
     }
 
     @Test
     public void searchTestMultipleMatches() {
+        Task[] expected = {t2, t3};
         Task[] result = todos.search("код");
-        Assertions.assertEquals(2, result.length);
+        Assertions.assertArrayEquals(expected, result);
     }
 
     @Test
     public void searchTestSingleMatch() {
+        Task[] expected = {t1};
         Task[] result = todos.search("продукты");
-        Assertions.assertEquals(1, result.length);
+        Assertions.assertArrayEquals(expected, result);
     }
 
     @Test
     public void searchTestNoMatches() {
+        Task[] expected = new Task[0];
         Task[] result = todos.search("выход");
-        Assertions.assertEquals(0, result.length);
+        Assertions.assertArrayEquals(expected, result);
     }
 
     @Test
